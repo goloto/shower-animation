4c470c340272f0c6d0e6bf11d6197b1f2c476978
Mikhail Goloto <goloto@pm.me>
2021-08-20 10:38:23 +0300

    add Hover slide

---
+++ slides/Hover/scripts.js
@@ -0,0 +1,7 @@
+function showHoverStyles() {
+  document.querySelector('#hover-styles').classList.add('show');
+}
+
+function hideHoverStyles() {
+  document.querySelector('#hover-styles').classList.remove('show');
+}
